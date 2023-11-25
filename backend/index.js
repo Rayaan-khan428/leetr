d84aad6ddd766c95bf33e1f61e953a8b2f85416f
Rayaan Khan <rayaan.k.ca@gmail.com>
2023-11-25 13:12:17 -0500

    restructured, added comments, TODO: add .env

--- backend/index.js
+++ backend/index.js
@@ -1,70 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const Problem = require('./Problem'); // Import the Problem model
+
 const app = express();
-mongoose.set('strictQuery', false)
-const Problem = require('./Problem');
 const router = express.Router();
-app.use(express.json()); // To parse JSON request bodies
-
-
 const PORT = 3000;
 
-// get all the problems /api/problems
+// Middleware to parse JSON request bodies
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Welcome to my LeetCode Tracker API!');
-});
-
-router.get('/test', async (req, res) => {
-    try {
-      const problems = await Problem.find();
-      res.json(problems);
-    } catch (err) {
-      res.status(500).json({ message: err.message });
-    }
-});
-
-// Add a new problem /api/problems
-router.post('/add', async (req, res) => {
-    const problem = new Problem({
-      // assuming req.body contains all the necessary fields
-      ...req.body
-    });
-  
-    try {
-      const newProblem = await problem.save();
-      res.status(201).json(newProblem);
-    } catch (err) {
-      res.status(400).json({ message: err.message });
-    }
-});
-  
-// Update an Existing Problem /api/problems/:id
-router.put('/:id', async (req, res) => {
-    try {
-      const updatedProblem = await Problem.findByIdAndUpdate(req.params.id, req.body, { new: true });
-      if (!updatedProblem) return res.status(404).json({ message: 'Problem not found' });
-      res.json(updatedProblem);
-    } catch (err) {
-      res.status(400).json({ message: err.message });
-    }
-});
-
-// Delete a Problem /api/problems/:id
-router.delete('/:id', async (req, res) => {
-    try {
-      const problem = await Problem.findByIdAndDelete(req.params.id);
-      if (!problem) return res.status(404).json({ message: 'Problem not found' });
-      res.json({ message: 'Problem deleted' });
-    } catch (err) {
-      res.status(500).json({ message: err.message });
-    }
-});
-
-app.use('/api/problems', router);
-
-const start = async() => {
-    
+// Connect to MongoDB
+mongoose.set('strictQuery', false);
+const start = async () => {
     try {
         await mongoose.connect('mongodb+srv://mongo:rayaan@cluster0.ezfpo5q.mongodb.net/LeetcodeQuestions?retryWrites=true&w=majority');
         app.listen(PORT, () => {
@@ -73,8 +20,54 @@ const start = async() => {
     } catch (err) {
         console.error('Database connection error:', err);
     }
-}
-  
-start();
-  
+};
 
+// Route to get all problems
+router.get('/', async (req, res) => {
+    try {
+        const problems = await Problem.find();
+        res.json(problems);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+// Route to add a new problem
+router.post('/add', async (req, res) => {
+    const problem = new Problem({ ...req.body });
+  
+    try {
+        const newProblem = await problem.save();
+        res.status(201).json(newProblem);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
+  
+// Route to update an existing problem
+router.put('/:id', async (req, res) => {
+    try {
+        const updatedProblem = await Problem.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (!updatedProblem) return res.status(404).json({ message: 'Problem not found' });
+        res.json(updatedProblem);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
+
+// Route to delete a problem
+router.delete('/:id', async (req, res) => {
+    try {
+        const problem = await Problem.findByIdAndDelete(req.params.id);
+        if (!problem) return res.status(404).json({ message: 'Problem not found' });
+        res.json({ message: 'Problem deleted' });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+// Mount the router on the '/api/problems' path
+app.use('/api/problems', router);
+
+// Start the server
+start();
