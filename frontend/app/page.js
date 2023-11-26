2669f6938b5fd2f90fff88336d019b3fed83fc4f
Umar Rasool <umarrasool15@gmail.com>
2023-11-25 20:34:39 -0500

    Plotly Pie Chart

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,12 +1,19 @@
+// page.js
 'use client'
 import React, { useState } from 'react';
 import PrimarySearchAppBar from "@/components/navbar";
 import ReactVirtualizedTable, { rows } from "@/components/table";
+import StatusPieChart from "@/components/pieChart"; // Ensure this path is correct
 
 export default function Home() {
   // Set the initial darkMode state to true
   const [darkMode, setDarkMode] = useState(true);
   const [searchQuery, setSearchQuery] = useState('');
+
+  // Dummy data for the StatusPieChart
+  const [attempted, setAttempted] = useState(50);
+  const [notAttempted, setNotAttempted] = useState(20);
+  const [successful, setSuccessful] = useState(30);
 
   const filteredData = rows.filter(item => item.title.toLowerCase().includes(searchQuery.toLowerCase()));
 
@@ -20,9 +27,16 @@ export default function Home() {
           onSearchChange={setSearchQuery}
         />
       </header>
-      <div className="flex-grow p-14">
+      <main className="flex-grow p-14">
+        {/* StatusPieChart component wrapped in a div for styling */}
+        <div className="pie-chart-container" style={{ width: '30%', height: 'auto' }}>
+  <StatusPieChart attempted={attempted} notAttempted={notAttempted} successful={successful} darkMode={darkMode} />
+</div>
+
+
+        {/* Your existing table */}
         <ReactVirtualizedTable data={filteredData} darkMode={darkMode} />
-      </div>
+      </main>
     </div>
   );
 }
