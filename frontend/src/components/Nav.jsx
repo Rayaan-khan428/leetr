8cf6c95cb1b5e46bcb7d46f5a75f42785ed8c0a3
Rayaan Khan <rayaan.k.ca@gmail.com>
2023-11-26 06:54:11 -0500

    refractored navbar code

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -12,29 +12,37 @@ import {
   Stack,
   useColorMode,
   Center,
-} from '@chakra-ui/react'
-import { MoonIcon, SunIcon } from '@chakra-ui/icons'
+} from '@chakra-ui/react';
+import { MoonIcon, SunIcon } from '@chakra-ui/icons';
 import { Link } from 'react-router-dom';
 
-const NavLink = ({ children }) => (
-  <Box
-    as="a"
-    px={2}
-    py={1}
-    rounded="md"
-    _hover={{
-      textDecoration: 'none',
-      bg: useColorModeValue('gray.200', 'gray.700'),
-    }}
-    href="#"
-  >
-    {children}
-  </Box>
-);
+function ThemeToggleButton() {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const icon = colorMode === 'light' ? <MoonIcon /> : <SunIcon />;
+  return <Button onClick={toggleColorMode}>{icon}</Button>;
+}
+
+function UserMenu() {
+  const avatarUrl = "https://avatars.dicebear.com/api/male/username.svg"; // Replace with your avatar logic
+
+  return (
+    <Menu>
+      <MenuButton as={Button} rounded="full" variant="link" cursor="pointer" minW={0}>
+        <Avatar size="sm" src={avatarUrl} />
+      </MenuButton>
+      <MenuList align="center">
+        <Center><Avatar size="2xl" src={avatarUrl} /></Center>
+        <Center><p>Username</p></Center>
+        <MenuDivider />
+        <MenuItem>Your Servers</MenuItem>
+        <MenuItem>Account Settings</MenuItem>
+        <MenuItem>Logout</MenuItem>
+      </MenuList>
+    </Menu>
+  );
+}
 
 export default function Nav() {
-  const { colorMode, toggleColorMode } = useColorMode();
-
   return (
     <Flex
       as="nav"
@@ -48,50 +56,14 @@ export default function Nav() {
       color={useColorModeValue('gray.600', 'white')}
     >
       <Link to="/">
-        <Box
-          fontFamily="'Orbitron', sans-serif"
-          fontWeight="700"
-          fontSize="40px"
-          display="inline-block"
-        >
+        <Box fontFamily="'Orbitron', sans-serif" fontWeight="700" fontSize="40px">
           Leetr
         </Box>
       </Link>
-      
-      <Stack direction="row" spacing={7} align="center">
-        <Button onClick={toggleColorMode}>
-          {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
-        </Button>
 
-        <Menu>
-          <MenuButton
-            as={Button}
-            rounded="full"
-            variant="link"
-            cursor="pointer"
-            minW={0}
-          >
-            <Avatar
-              size="sm"
-              src="https://avatars.dicebear.com/api/male/username.svg"
-            />
-          </MenuButton>
-          <MenuList align="center">
-            <Center>
-              <Avatar
-                size="2xl"
-                src="https://avatars.dicebear.com/api/male/username.svg"
-              />
-            </Center>
-            <Center>
-              <p>Username</p>
-            </Center>
-            <MenuDivider />
-            <MenuItem>Your Servers</MenuItem>
-            <MenuItem>Account Settings</MenuItem>
-            <MenuItem>Logout</MenuItem>
-          </MenuList>
-        </Menu>
+      <Stack direction="row" spacing={7} align="center">
+        <ThemeToggleButton />
+        <UserMenu />
       </Stack>
     </Flex>
   );
