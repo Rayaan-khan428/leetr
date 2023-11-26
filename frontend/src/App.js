db1fce65fa5dd1aee032d652779e74a1cee8dc16
Rayaan Khan <rayaan.k.ca@gmail.com>
2023-11-25 22:51:31 -0500

    ProblemsTable Added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import { ChakraProvider } from '@chakra-ui/react'
+import ProblemsTable from './components/ProblemsTable';
 
 function App() {
   return (
     <ChakraProvider>
-      
+      <ProblemsTable/>
     </ChakraProvider>
   );
 }
