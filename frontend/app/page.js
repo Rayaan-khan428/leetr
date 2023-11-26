42c4de4ec2a601a96e7f2a07765f0fd90a76c4b1
Rayaan Khan <rayaan.k.ca@gmail.com>
2023-11-25 21:31:10 -0500

    pushing

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,28 +1,41 @@
-'use client'
+'use client';
 import React, { useState } from 'react';
 import PrimarySearchAppBar from "@/components/PrimarySearchAppBar";
 import ReactVirtualizedTable, { rows } from "@/components/ReactVirtualizedTable";
 
 export default function Home() {
-  // Set the initial darkMode state to true
+  // State for managing dark mode
   const [darkMode, setDarkMode] = useState(true);
+
+  // State for handling search query
   const [searchQuery, setSearchQuery] = useState('');
 
-  const filteredData = rows.filter(item => item.title.toLowerCase().includes(searchQuery.toLowerCase()));
+  // Function to filter data based on search query
+  const getFilteredData = () => {
+    return rows.filter(item => 
+      item.title.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+  };
+
+  // Filtered data for the table
+  const filteredData = getFilteredData();
 
   return (
     <div className={`flex flex-col min-h-screen w-full ${darkMode ? 'bg-[rgb(26,26,26)] text-white' : 'bg-white text-black'}`}>
+      {/* Header with Search Bar */}
       <header className="w-full">
         <PrimarySearchAppBar 
           darkMode={darkMode} 
-          handleThemeChange={() => setDarkMode(!darkMode)}
+          handleThemeChange={() => setDarkMode(prevMode => !prevMode)}
           searchQuery={searchQuery}
           onSearchChange={setSearchQuery}
         />
       </header>
-      <div className="flex-grow p-14">
+
+      {/* Main Content with Virtualized Table */}
+      <main className="flex-grow p-14">
         <ReactVirtualizedTable data={filteredData} darkMode={darkMode} />
-      </div>
+      </main>
     </div>
   );
 }
